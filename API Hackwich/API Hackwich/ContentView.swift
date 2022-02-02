77b5d9eb312c62f52d56c3886a542ab7f3df9b81
Victor <victor>
2022-02-02 10:35:18 -0600

    Got API key

--- API Hackwich/API Hackwich/ContentView.swift
+++ API Hackwich/API Hackwich/ContentView.swift
@@ -23,16 +23,11 @@ struct ContentView: View {
             .onAppear(perform: {
                         getJokes()
                     })
-
+            
         }
     }
     func getJokes() {
-        jokes.append(Joke(setup: "Why do programmers always get Christmas and Halloween mixed up?",
-                          punchline: "Because DEC 25 = OCT 31"))
-        jokes.append(Joke(setup: "How did the programmer die in the shower?",
-                          punchline: "She followed the shampoo bottle instructions: Lather. Rinse. Repeat."))
-        jokes.append(Joke(setup: "There are 10 types of people in the world",
-                          punchline: "Those who understand binary and those who don’t."))
+        let apiKey = "?rapidapi-key=98ee719c29msh95601a79921ea8fp170b67jsnbb2227354604"
     }
 }
 
