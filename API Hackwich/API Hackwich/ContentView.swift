859a89c8d6fc0d0f558c94957ecf83fca414a957
Victor <victor>
2022-02-16 10:59:34 -0600

    finish

--- API Hackwich/API Hackwich/ContentView.swift
+++ API Hackwich/API Hackwich/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var jokes = [Joke]()
+    @State private var showingAlert = false
     var body: some View {
         NavigationView {
             List(jokes) { joke in
@@ -21,13 +22,35 @@ struct ContentView: View {
             }
             .navigationTitle("Programming Jokes")
             .onAppear(perform: {
-                        getJokes()
-                    })
-            
+                getJokes()
+            })
+            .alert(isPresented: $showingAlert, content: {
+                Alert(title: Text("Loading Error"),
+                      message: Text("There was a problem loading the data"),
+                      dismissButton: .default(Text("OK")))
+            })
         }
     }
+    
     func getJokes() {
         let apiKey = "?rapidapi-key=98ee719c29msh95601a79921ea8fp170b67jsnbb2227354604"
+        let query = "https://dad-jokes.p.rapidapi.com/joke/type/programming\(apiKey)"
+        if let url = URL(string: query) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                if json["success"] == true {
+                    let contents = json["body"].arrayValue
+                    for item in contents {
+                        let setup = item["setup"].stringValue
+                        let punchline = item["punchline"].stringValue
+                        let joke = Joke(setup: setup, punchline: punchline)
+                        jokes.append(joke)
+                    }
+                    return
+                }
+            }
+        }
+        showingAlert = true
     }
 }
 
